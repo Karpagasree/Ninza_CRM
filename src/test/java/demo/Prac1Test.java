d58108f468037e79fcdea01c2524852ffd2d193c
ROSHAN KUMAR <ROSHAN KUMAR@LAPTOP-H6VK64VB>
2025-12-18 07:52:32 +0530

    First Time Committed

---
+++ src/test/java/demo/Prac1Test.java
@@ -0,0 +1,13 @@
+package demo;
+
+import org.testng.annotations.Test;
+
+public class Prac1Test {
+	
+	@Test(groups = "smoke")
+	public void carTest() 
+	{
+		System.out.println("Audi");
+	}
+
+}
---
+++ src/test/java/demo/PracTest.java
@@ -0,0 +1,13 @@
+package demo;
+
+import org.testng.annotations.Test;
+
+public class PracTest {
+	
+	@Test(groups = "regression")
+	public void bikeTest()
+	{
+		System.out.println("Yamaha");
+	}
+
+}
