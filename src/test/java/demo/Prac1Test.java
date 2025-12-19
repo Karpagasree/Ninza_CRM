d8953d59b486c5889ce0772e5f9c7a4356f3234b
ROSHAN KUMAR <ROSHAN KUMAR@LAPTOP-H6VK64VB>
2025-12-19 08:48:15 +0530

    Hyundai Committed

--- src/test/java/demo/Prac1Test.java
+++ src/test/java/demo/Prac1Test.java
@@ -8,7 +8,7 @@ public class Prac1Test {
 	public void carTest() 
 	{
 		System.out.println("Audi");
-		System.out.println("Benz");
+		System.out.println("Hyundai");
 		System.out.println("BMW");
 	}
 
